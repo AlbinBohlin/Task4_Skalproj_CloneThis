6182a8a82902bbfc78001845e0310b3c31693868
Albin <albin@bohlin.nu>
2025-06-12 13:15:02 +0200

    EmaineList done

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -9,28 +9,42 @@
         /// The main method, vill handle the menues for the program
         /// </summary>
         /// <param name="args"></param>
+        /// 
+
+
+        static string ValidateInput()
+        {
+            string input;
+            try
+            {
+                input = Console.ReadLine();
+                if (input.Length != 0) return input;
+                return "Wrong";
+
+            }
+            catch (Exception) //If the input line is empty, we ask the users for some input.
+            {
+                Console.Clear();
+                return "gg";
+            }
+        }
+
         static void Main()
         {
 
             while (true)
             {
+
                 Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
                     + "\n1. Examine a List"
                     + "\n2. Examine a Queue"
                     + "\n3. Examine a Stack"
                     + "\n4. CheckParenthesis"
                     + "\n0. Exit the application");
-                char input = ' '; //Creates the character input to be used with the switch-case below.
-                try
-                {
-                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
-                }
-                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
-                {
-                    Console.Clear();
-                    Console.WriteLine("Please enter some input!");
-                }
-                switch (input)
+
+                string input = ValidateInput();
+
+                switch (input[0])
                 {
                     case '1':
                         ExamineList();
@@ -73,12 +87,34 @@
              * Below you can see some inspirational code to begin working.
             */
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+            List<string> theList = [];
+            Console.WriteLine("+... Adds\n-... Removes\n0 Return to main");
 
-            //switch(nav){...}
+            while (true)
+            {
+
+                string? input = ValidateInput();
+
+                string msg;
+                switch (input[0])
+                {
+                    case '+':
+                        theList.Add(input[1..]);
+                        msg = $"Added \"{input[1..]}\"";
+                        break;
+                    case '-': msg = theList.Remove(input[1..]) ? $"Removed first instance of \"{input[1..]}\"" :$"Failed to remove \"{input[1..]}\""; break;
+                    case '0': msg = "Returning to MainMenu"; Console.WriteLine(msg); return;
+                    default: msg = "Must have +,-,0 as first char in string"; break;
+                }
+                Console.WriteLine(PrintListData(theList, msg));
+
+            }
+
+            static string PrintListData(List<string> list, string msg)
+            {
+                return $"Mesage: {msg}\nListLength: {list.Count}\tSize: {list.Capacity}";
+            }
+
         }
 
         /// <summary>
@@ -104,7 +140,7 @@
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
         }
-
+#nullable disable
         static void CheckParanthesis()
         {
             /*
@@ -113,28 +149,28 @@
              * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
              */
 
-
-
             //börja med input.. vi tar exemplet från ovan och jobbar med..
-            string? input = File.ReadAllText("CheckParanthesis.txt");
+            //string? input = File.ReadAllText("CheckParanthesis.txt");
+            string input = Console.ReadLine();
 
-
-
-            Console.WriteLine("input: "+input+"\nChecking input...");
+            Console.WriteLine("input: " + input + "\nChecking input...");
             //parsa ut intressant substräng, de tecken vi bryr oss om: (,[,{,),],}
             char[] parenthesis = ['(', '[', '{', ')', ']', '}'];
-           
-            int start = input.IndexOfAny(parenthesis);// -1 if none
-            input = input[start..];
 
+            int start = input.IndexOfAny(parenthesis);// -1 if none
+            try
+            {
+                input = input[start..];//Empty string or no parenthesis
+            }
+            catch (Exception) { }
 
             Console.WriteLine(Check(input, parenthesis, 0, new StringBuilder()));//Utvärdera input
 
             //nästla en metod som kör sig själv rekursivt.
-            static string Check(string? input, char[] parenthesis, int depth, StringBuilder result)
+            static string Check(string input, char[] parenthesis, int depth, StringBuilder result)
             {
                 //sluta om vi hittar en som stänger
-                if (parenthesis[3..].Contains(input.First()))
+                if (input.Length != 0 && parenthesis[3..].Contains(input.First()))
                 {
                     result.Append(input.First());
                     depth--;
@@ -166,30 +202,29 @@
                         result.Clear();
                         return Check(input, parenthesis, 0, result);//Fortsätt
                     }
-                    return Check(input, parenthesis, depth, result);//Fortsätt efter korrekt
-
+                    return Check(input, parenthesis, depth, result);//Fortsätt efter 
                 }
-
                 try
                 {
-                result.Append(input.First());
-                input = input[1..];
+                    result.Append(input.First());
+                    input = input[1..];
                     input = input[input.IndexOfAny(parenthesis)..];
                 }
-                catch (Exception e) { 
+                catch (Exception e)
+                {
                     //Antingen färdig eller en eller fler ostängda inledande paranteser.
-                    if(result.Length>1) Console.WriteLine("Error with unclosed parenthesis: "+ result.ToString());
-                    return "\n-------All done----------\n"; }
+                    if (result.Length > 1) Console.WriteLine("Error with unclosed parenthesis: " + result.ToString());
+                    return "\n-------All done----------\n";
+                }
                 //annars upprepa processen
                 depth++;//Räkna inledande paranteser i följd.
                 return Check(input, parenthesis, depth, result);
             }
-           
+
 
 
             static bool Match(char left, char right)
             {
-                
                 switch (left)
                 {
                     case '(': return right.Equals(')');
@@ -197,8 +232,6 @@
                     case '{': return right.Equals('}');
                     default: return false;
                 }
-
-
             }
 
 
@@ -206,5 +239,6 @@
         }
 
     }
-}
 
+
+}
